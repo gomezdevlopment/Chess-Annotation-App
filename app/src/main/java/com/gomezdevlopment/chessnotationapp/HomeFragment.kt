0c209948343c1aa79d1ec5fa8018ee681156e3c5
Pablo Nathanielle Gomez <gomezdevlopment@gmail.com>
2022-05-04 22:43:04 -0400

    Create adapter for chess moves recycler

--- app/src/main/java/com/gomezdevlopment/chessnotationapp/HomeFragment.kt
+++ app/src/main/java/com/gomezdevlopment/chessnotationapp/HomeFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.gomezdevlopment.chessnotationapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +20,27 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val whiteAnnotations: ArrayList<String> = arrayListOf("e4", "d4")
+        val blackAnnotations: ArrayList<String> = arrayListOf("e5", "d5")
+
+        val whiteMovesRecycler = binding.whiteMovesRecycler
+        val blackMovesRecycler = binding.blackMovesRecycler
+
+        whiteMovesRecycler.layoutManager = LinearLayoutManager(view.context)
+        val whiteMovesAdapter = MoveAdapter(whiteAnnotations)
+        whiteMovesRecycler.adapter = whiteMovesAdapter
+
+        blackMovesRecycler.layoutManager = LinearLayoutManager(view.context)
+        val blackMovesAdapter = MoveAdapter(blackAnnotations)
+        blackMovesRecycler.adapter = blackMovesAdapter
+
     }
 }
---
+++ app/src/main/java/com/gomezdevlopment/chessnotationapp/MoveAdapter.kt
@@ -0,0 +1,29 @@
+package com.gomezdevlopment.chessnotationapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MoveAdapter(val notations: ArrayList<String>): RecyclerView.Adapter<MoveAdapter.MyViewHolder>() {
+
+
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val annotation: TextView = itemView.findViewById(R.id.annotation)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoveAdapter.MyViewHolder {
+        val moveItem = LayoutInflater.from(parent.context).inflate(R.layout.move_item, parent, false)
+        return MyViewHolder(moveItem)
+    }
+
+    override fun onBindViewHolder(holder: MoveAdapter.MyViewHolder, position: Int) {
+        val currentNotation = "${position+1}. ${notations[position]}"
+        holder.annotation.text = currentNotation
+    }
+
+    override fun getItemCount(): Int {
+        return notations.size
+    }
+
+}
