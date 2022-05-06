dcdce2c00842e19cafd264e5cc7498f86a013cc2
Pablo Nathanielle Gomez <gomezdevlopment@gmail.com>
2022-05-05 21:15:18 -0400

    Create button and logic to create a new game after warning dialog shown to user

--- app/src/main/java/com/gomezdevlopment/chessnotationapp/HomeFragment.kt
+++ app/src/main/java/com/gomezdevlopment/chessnotationapp/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.gomezdevlopment.chessnotationapp
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -41,5 +43,27 @@ class HomeFragment : Fragment() {
         blackMovesRecycler.layoutManager = LinearLayoutManager(view.context)
         val blackMovesAdapter = MoveAdapter(blackAnnotations)
         blackMovesRecycler.adapter = blackMovesAdapter
+
+        binding.newGameButton.setOnClickListener {
+            createNewGameAlert(view.context)
+        }
+    }
+
+    private fun createNewGameAlert(context: Context){
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Create a new game?")
+        builder.setMessage("The current game will be deleted.")
+
+        builder.setPositiveButton("Yes") { dialog, which ->
+            binding.blackMovesRecycler.adapter?.notifyItemRangeRemoved(0, blackAnnotations.size)
+            binding.whiteMovesRecycler.adapter?.notifyItemRangeRemoved(0, whiteAnnotations.size)
+            whiteAnnotations.clear()
+            blackAnnotations.clear()
+        }
+
+        builder.setNegativeButton("Cancel") { dialog, which ->
+
+        }
+        builder.show()
     }
 }
