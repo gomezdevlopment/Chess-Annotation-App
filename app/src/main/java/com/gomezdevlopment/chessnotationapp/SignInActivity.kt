039069fd316e4dff7338ccc97b4a050600c9d6c8
Pablo Nathanielle Gomez <gomezdevlopment@gmail.com>
2022-05-07 19:35:21 -0400

    Add logic for firebase sign up and sign in

--- app/src/main/java/com/gomezdevlopment/chessnotationapp/SignInActivity.kt
+++ app/src/main/java/com/gomezdevlopment/chessnotationapp/SignInActivity.kt
@@ -1,16 +1,55 @@
 package com.gomezdevlopment.chessnotationapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.gomezdevlopment.chessnotationapp.databinding.SignInLayoutBinding
+import com.google.firebase.auth.FirebaseAuth
 
-class SignInActivity: AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: SignInLayoutBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = SignInLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.submitButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_LONG).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Fields cannot be left empty!", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+        binding.signUpButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(firebaseAuth.currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
