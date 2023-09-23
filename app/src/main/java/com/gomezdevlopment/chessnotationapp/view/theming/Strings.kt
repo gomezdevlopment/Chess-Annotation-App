c33ccfbdc5f83d0f440df97012792013f51dce97
Nathan Gomez <gomezdevlopment@gmail.com>
2023-09-23 18:54:08 -0400

    Update target android version and increase version number

--- app/src/main/java/com/gomezdevlopment/chessnotationapp/view/theming/Strings.kt
+++ app/src/main/java/com/gomezdevlopment/chessnotationapp/view/theming/Strings.kt
@@ -1,4 +1,4 @@
 package com.gomezdevlopment.chessnotationapp.view.theming
 
-const val appVersion = "1.02"
+const val appVersion = "1.05"
 const val privacyPolicy = "https://www.app-privacy-policy.com/live.php?token=pBm4QNdAXctDlRBnn76zxbQiLIuX9iiv"
